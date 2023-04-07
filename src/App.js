a1a9f2cfef2e3169e885fd85f15406347031a53b
ShraddhaJ11 <shraddhaj1103@gmail.com>
2023-04-07 10:54:59 +0530

    create custom hook for api calls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./styles.css";
 import useOnline from "./useOnline";
+import { useFetch } from "./useFetch";
 
 export default function App() {
   const isOnline = useOnline();
+  const {response,loading,error} = useFetch('https://dummyjson.com/products',{});
+
+  if(loading)
+  return <div>Loading...</div>
+
+  if(error)
+  return <div>{error}</div>
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>{isOnline ? "Online" : "Offline"}</h2>
+      <div>{response.products}</div>
     </div>
   );
 }
